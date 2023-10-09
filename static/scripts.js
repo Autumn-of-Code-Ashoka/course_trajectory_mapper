d2b15ca9d44d0818838f24e7b971af77e9e95975
Ruzil357 <rushilgupta4@gmail.com>
2023-10-10 02:23:09 +0530

    Added search bar (Someone please fix the CSS)

--- static/scripts.js
+++ static/scripts.js
@@ -23,3 +23,49 @@ function drop(ev) {
 
   target.appendChild(elem);
 }
+
+async function updateCourses() {
+  const query = document.querySelector('#courseQuery').value;
+  const courses = await fetch(`/courses?q=${query}`).then((res) => res.json());
+
+  const container = document.querySelector('#courseContainer');
+  container.innerHTML = '';
+
+  let selectedCourses = document.getElementsByName('courseDiv');
+  selectedCourses = Array.from(selectedCourses).map((course) => course.id);
+  console.log(selectedCourses);
+
+  courses.forEach((course) => {
+    if (selectedCourses.includes(course.code)) {
+      return;
+    }
+
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.setAttribute('name', 'courseDiv');
+    div.setAttribute('draggable', 'true');
+    div.ondragstart = (event) => drag(event);
+    div.setAttribute('id', course.code);
+
+    const div2 = document.createElement('div');
+    div2.classList.add('border', 'border-black', 'my-1', 'py-2', 'rounded-1');
+
+    const courseName = document.createElement('h5');
+    courseName.classList.add('text-center', 'm-0');
+    courseName.textContent = course.name;
+
+    const courseCode = document.createElement('p');
+    courseCode.classList.add('text-center', 'm-0', 'p-0');
+    courseCode.textContent = course.code;
+
+    // append
+    div2.appendChild(courseName);
+    div2.appendChild(courseCode);
+    div.appendChild(div2);
+    container.appendChild(div);
+  });
+}
+
+window.onload = () => {
+  updateCourses();
+};
--- trajectory.py
+++ trajectory.py
@@ -1,15 +1,36 @@
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+with open("courses.json", "r") as f:
+    COURSES = json.load(f)
 
-@app.route("/")
+
+@app.route("/", methods=["GET"])
 def hello_world():
-    with open("courses.json", "r") as f:
-        courses = json.load(f)
+    return render_template("index.html")
 
-    return render_template("index.html", courses=courses)
+
+@app.route("/courses", methods=["GET"])
+def courses():
+    query = request.args.get("q")
+    if not query:
+        return jsonify(COURSES)
+
+    final_courses = []
+
+    for course in COURSES:
+        if any(
+            [
+                query.lower().strip() in i.lower()
+                for i in course.values()
+                if type(i) == str
+            ]
+        ):
+            final_courses.append(course)
+
+    return jsonify(final_courses)
 
 
 if __name__ == "__main__":
